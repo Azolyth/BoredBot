7d162dc70822764d1736cd4d66f2675b17d67bc5
Silas <azolyth@gmail.com>
2022-08-04 01:51:23 -0700

    Refactored button handler

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ document.getElementById('activity-button').addEventListener('click', () => {
   fetch('https://apis.scrimba.com/bored/api/activity')
     .then(response => response.json())
     .then(data => {
-      document.getElementById('activity').textContent = `${data.activity}`;
-      console.log(data.activity);
+      document.getElementById('activity').textContent = data.activity;
     });
 });
